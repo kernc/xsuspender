8fadc2b44aa49ef5b4b4b58ad1c0daf439a639ad
kernc <kerncece@gmail.com>
2020-11-06 05:10:18 +0100

    MNT: Migrate CI from Travis CI to GitHub Actions (#22)
    * MNT: Migrate CI from Travis CI to GitHub Actions
    
    * ensure python3/wheel is present
    
    * relax cppcheck and cpplint a bit. seems after the update they went awry
    
    * debug what passes locally
    
    * god damn it why can't this ever work
    
    * it was this bug: https://trac.cppcheck.net/ticket/7792
    
    * *correctly* bump OS to Ubuntu 20.04
    
    * join build & test steps. gha seems to not retain pwd across steps?
    
    * only lint in 'gcc' test
    
    * don't install gcc. it already there

--- lint.sh
+++ lint.sh
@@ -7,10 +7,14 @@ PATH="$PATH:$HOME/.local/bin"
 
 error () { echo "ERROR: $@" >&2; exit 1; }
 
-cppcheck --enable=all --error-exitcode=1 \
-    --suppress=unusedFunction:"$ROOT/src/macros.h" "$ROOT"/src/*.[ch]
+cppcheck --error-exitcode=1 \
+    --enable=warning,performance,portability,unusedFunction,missingInclude \
+    --suppress=unusedFunction:"$ROOT/src/macros.h" \
+    "$ROOT"/src/*.[ch]
 
-cpplint --counting=detailed --recursive "$ROOT"/src
+cpplint --counting=detailed --recursive \
+    --filter=-build/endif_comment \
+    "$ROOT"/src
 
 grep -RF $'\t' "$ROOT"/src &&
     error 'Tabs found. Use spaces.' || echo 'Whitespace OK'
